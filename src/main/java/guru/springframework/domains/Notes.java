31d0e1423f74e8d9455d3c968c56402d6fc93a40
Joydeep <joydeep.chowdhury@oracle.com>
2021-09-17 00:51:53 +0530

    Added entities

---
+++ src/main/java/guru/springframework/domains/Notes.java
@@ -0,0 +1,45 @@
+package guru.springframework.domains;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipieNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipieNotes() {
+        return recipieNotes;
+    }
+
+    public void setRecipieNotes(String recipieNotes) {
+        this.recipieNotes = recipieNotes;
+    }
+}
